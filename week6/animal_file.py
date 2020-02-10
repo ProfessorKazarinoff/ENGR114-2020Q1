bb2964b498eb601d2f33ac401b09acf987fd6494
SYAM106u8249 <SYAM106u8249@ad.pcc.edu>
2020-02-10 12:01:22 -0800

    week6 notes and Exam1 review added

---
+++ week6/animal_file.py
@@ -0,0 +1,9 @@
+# animal_file.py
+
+def cats_func(n):
+    """ a function to return the number of cats"""
+    return f"{n} cats"
+
+def dogs_func(n):
+    """ a function to return the number of dogs"""
+    return f"{n} dogs"
---
+++ week6/vehicals_file.py
@@ -0,0 +1,9 @@
+# vehicals_files.py
+
+def cars_func(n):
+    "a function that returns the number of cars and the number of wheels"
+    return n, "cars"
+
+def trucks_func(n_trucks, n_wheels):
+    "a function thtat returns the number of trucks and the number of wheels assuming the trucks are 18 wheelers"
+    return n_trucks, n_trucks*n_wheels
